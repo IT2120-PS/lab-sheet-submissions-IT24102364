429f1829072029abca1eaae33ec48c470b5bb841
IT24102364 <it24102364@my.sliit.lk>
2025-09-15 10:02:36 +0530

    Lab sheet 6

---
+++ Lab Sheet 06/IT24102364.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\asind\\Desktop\\IT24102364")
+
+#Question 1
+#Part 1
+#Binomial Distribution
+
+#Part 2
+1 - pbinom(46,50,0.85, lower.tail = TRUE)
+
+
+#Question 2
+#Part 1
+#X - Number of calls received by a call center per hour.
+
+#Part 2
+#Poisson Distribution
+
+#Part 3
+dpois(15,12)
